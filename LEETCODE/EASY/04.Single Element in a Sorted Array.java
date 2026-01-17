e5c1e789c1504b4b86d0416e4128ee7e5eabd3a5
savisalanjala17 <savisalanjala17@gmail.com>
2026-01-17 18:41:18 +0530

    Create 04.Single Element in a Sorted Array.java

---
+++ LEETCODE/EASY/04.Single Element in a Sorted Array.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<right){
+
+            int mid=left+(right-left)/2;
+
+            if((mid%2!=0 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
+              left=mid+1;
+
+           else right=mid;
+
+       
+    }
+
+     return nums[left]; 
+    }
+}
+
+
+540. Single Element in a Sorted Array
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
